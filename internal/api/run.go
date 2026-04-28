d0981ed3a5271813be2ca97898b5dd7f1afbb026
Del <matteo.tutti@epitech.eu>
2026-04-28 14:48:24 +0000

    feat(upload): add the upload endpoint

--- internal/api/run.go
+++ internal/api/run.go
@@ -10,24 +10,79 @@ package api
 import (
 	"context"
 	"fmt"
-	"time"
+	"github.com/matteoepitech/flick/internal/api/utils"
+	"github.com/quic-go/quic-go/http3"
+	"io"
+	"net/http"
+	"os"
 )
 
-// Run: Run the API.
+// Where the data is stored
+var dataDir string
+
+// uploadFileHandler: The upload file handler.
 //
 // Params:
-// - ctx (context.Context): The context of the main
+// - w (http.ResponseWriter): The write channel.
+// - r (*http.Request): The request headers informations.
+func uploadFileHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "This endpoint is meant to be POST only", http.StatusNotFound)
+		return
+	}
+
+	r.ParseMultipartForm(100 << 20)
+
+	file, header, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, "Cannot parse the file", http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	dst, err := os.Create(dataDir + "/" + header.Filename)
+	if err != nil {
+		http.Error(w, "Cannot save the file", http.StatusInternalServerError)
+		return
+	}
+	defer dst.Close()
+
+	fileBytes, err := io.Copy(dst, file)
+	if err != nil {
+		http.Error(w, "Error while copying the file", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Printf("[API]: Received a file of %d bytes\n", fileBytes)
+}
+
+// Run: Run the API on HTTP/3 (QUIC).
+//
+// Params:
+// - ctx (context.Context): The context of the main.
 //
 // Returns:
 // - result1 (error): nil if no error, otherwise an error.
 func Run(ctx context.Context) error {
-	fmt.Println("API: start")
+	http.HandleFunc("/upload", uploadFileHandler)
 
-	select {
-	case <-time.After(2 * time.Second):
-		fmt.Println("API: done")
-		return nil
-	case <-ctx.Done():
-		return fmt.Errorf("api canceled: %w", ctx.Err())
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return fmt.Errorf(utils.Red+"cannot get home dir: %w"+utils.Reset, err)
 	}
+
+	dataDir = homeDir + "/.flick/data" // global var
+	err = os.MkdirAll(dataDir, 0755)
+	if err != nil {
+		return fmt.Errorf(utils.Red + "[API]: cannot create the directory /opt/flick/data/" + utils.Reset)
+	}
+
+	fmt.Println("[API]: Starting FLICK server on :15702...")
+
+	err = http3.ListenAndServeTLS(":15702", "certificates/cert.pem", "certificates/key.pem", nil)
+	if err != nil {
+		return fmt.Errorf(utils.Red+"[API]: server error: %w"+utils.Reset, err)
+	}
+
+	return nil
 }
---
+++ internal/api/utils/colors.go
@@ -0,0 +1,15 @@
+/*
+** FLICK PROJECT, 2026
+** flick/internal/api/utils/colors
+** File description:
+** Utils colors file
+ */
+
+package utils
+
+const (
+	Red   = "\033[31m"
+	Green = "\033[32m"
+	Bold  = "\033[1m"
+	Reset = "\033[0m"
+)
