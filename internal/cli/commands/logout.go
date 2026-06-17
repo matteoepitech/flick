d54f3daf744f576bb4de5ef12a297ba9052d43d2
Del <matteo.tutti@epitech.eu>
2026-06-17 12:30:25 +0000

    feat(cli): add logout command on CLI

---
+++ internal/cli/commands/logout.go
@@ -0,0 +1,53 @@
+/*
+** FLICK PROJECT, 2026
+** flick/internal/cli/commands/login
+** File description:
+** Login flick source
+ */
+
+package commands
+
+import (
+	"fmt"
+
+	"github.com/matteoepitech/flick/internal/api/utils"
+	"github.com/matteoepitech/flick/internal/cli/config"
+	"github.com/spf13/cobra"
+)
+
+// logout CMD using cobra
+var logoutCmd = &cobra.Command{
+	Use:   "logout",
+	Short: "Logout your account",
+	RunE:  RunLogout,
+}
+
+// init: Init of the package goes here.
+func init() {
+	rootCmd.AddCommand(logoutCmd)
+}
+
+// RunLogout: Logout.
+//
+// Params:
+// - cmd (*cobra.Command): The Cobra command.
+// - args ([]string): Command arguments.
+//
+// Returns:
+// - error: An error if occurred.
+func RunLogout(cmd *cobra.Command, args []string) error {
+	creds, err := config.LoadCredentials()
+	if err != nil || creds == nil || creds.Token == "" || creds.UserID == "" {
+		fmt.Printf("You are not logged in.\n")
+		return nil
+	}
+
+	creds.Token = ""
+	if err := config.SaveCredentials(*creds); err != nil {
+		return fmt.Errorf("you are not able to be logged out.")
+	}
+
+	fmt.Printf(utils.Dim + "You are now logged out.\n" + utils.Reset)
+	return nil
+
+}
